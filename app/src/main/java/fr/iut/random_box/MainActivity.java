39bce551de01a3559b2617e49b5f3beedff76bae
Xiumin Lin <xiumin.lin@laposte.net>
2021-03-04 03:19:29 +0100

    Setup static Random Color Box + loading remote url image

--- app/src/main/java/fr/iut/random_box/MainActivity.java
+++ app/src/main/java/fr/iut/random_box/MainActivity.java
@@ -5,18 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnN, btnC, btnM;
@@ -42,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
         this.btnM.setText(R.string.movie);
     }
 
+    /**
+     * Affiche dans une popup le contenu aléatoire d'une boîte lorsque l'user le click
+     * @param view : la view qui a appelé la methode
+     */
     public void onClickBtnBox(View view){
         String box_name = "";
 
@@ -50,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.box_2: box_name += "color"; break;
             case R.id.box_3: box_name += "meal"; break;
             case R.id.box_4: box_name += "movie"; break;
-            default: return;
+            default: Log.d("rb", "Unknown box id"); return;
         }
         //increment stat of the selected box
         db.child("stats").child("box").child(box_name).setValue(ServerValue.increment(1));
@@ -61,30 +64,54 @@ public class MainActivity extends AppCompatActivity {
 
         //create box popup builder
         AlertDialog.Builder builder = new AlertDialog.Builder(this); //activity
-        //popup.setTitle(box_name.substring(0, 1).toUpperCase() + box_name.substring(1));   //Capitalize
-        //builder.setTitle(box_name.toUpperCase());
 
         // set the custom layout
         final View popupView = getLayoutInflater().inflate(R.layout.popup,null);
         builder.setView(popupView);
-        TextView title = popupView.findViewById(R.id.txtPopupTitle);
 
-        LinearLayout popupLinearLayout = (LinearLayout) popupView.findViewById(R.id.popupLayout);
-        Log.d("aaa", popupLinearLayout.toString());
+        //update custom layout components
+        TextView title = popupView.findViewById(R.id.txtPopTitle);
+        title.setText(box_name);
         switch (view.getId()){
             case R.id.box_1:
-                TextView txtRandNumber = new TextView(this);
-                String randNum = "" + Box.getRandomNumber();
+                TextView txtRandNumber = popupView.findViewById(R.id.txtPopBigNumber);
+                String randNum = "" + RandomBox.getRandomNumber();
                 txtRandNumber.setText(randNum);
-                txtRandNumber.setTextAppearance(R.style.rand_number);
-                popupLinearLayout.addView(txtRandNumber);
+                txtRandNumber.setVisibility(View.VISIBLE); break;
             case R.id.box_2:
+                ImageView imgRandColor = popupView.findViewById(R.id.imgPopContent);
+                //convert px to dp
+                int _200dpInpx = (int) (200 * popupView.getResources().getDisplayMetrics().density);
+                Log.d("rb", "200 dp = " + _200dpInpx + " px");
+                imgRandColor.getLayoutParams().height = _200dpInpx; //height receive px
+                imgRandColor.getLayoutParams().width = _200dpInpx;
+                imgRandColor.setVisibility(View.VISIBLE);
+
+                int randColor = RandomBox.getRandomColor();
+                imgRandColor.setBackgroundColor(randColor);
+                TextView txtCTitle = popupView.findViewById(R.id.txtPopContentTitle);
+                txtCTitle.setText("HEX = " + String.format("#%06X", (0xFFFFFF & randColor))); //covert int color to hex format
+                txtCTitle.setVisibility(View.VISIBLE); break;
             case R.id.box_3:
-            case R.id.box_4:
-                title.setText(box_name);; break;
+                ImageView imgContent = popupView.findViewById(R.id.imgPopContent);
+                String url = "https://cdn.myanimelist.net/images/anime/13/50521.jpg";
+                LoadImage loadImage = new LoadImage(imgContent);
+                loadImage.execute(url);
+                imgContent.setVisibility(View.VISIBLE);
+
+                TextView imgTitle = popupView.findViewById(R.id.txtPopTitle);
+                imgTitle.setText("Hyouka");
+                imgTitle.setVisibility(View.VISIBLE);
+
+                TextView subTitle = popupView.findViewById(R.id.txtPopSubTitle);
+                subTitle.setText("氷菓");
+                subTitle.setVisibility(View.VISIBLE);
+
+                View scrollView = popupView.findViewById(R.id.scrollPopDetail);
+                scrollView.setVisibility(View.VISIBLE);
+            case R.id.box_4: break;
             default: return;
         }
-
         if(view.getId() == R.id.box_3 || view.getId() == R.id.box_4){
             builder.setNeutralButton("Get More Info", new DialogInterface.OnClickListener() {
                 @Override
@@ -94,7 +121,10 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        //display popup
         AlertDialog popup = builder.create();
         popup.show();
     }
+
 }
