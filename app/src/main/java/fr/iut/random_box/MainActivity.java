605fabe36b5406f7ac9a1d335e622bd5ffedf91a
epg1213 <44379863+epg1213@users.noreply.github.com>
2021-03-13 14:22:46 +0100

    Update MainActivity.java

--- app/src/main/java/fr/iut/random_box/MainActivity.java
+++ app/src/main/java/fr/iut/random_box/MainActivity.java
@@ -1,258 +1,209 @@
 package fr.iut.random_box;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.media.MediaPlayer;
-import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.Volley;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ServerValue;
-import com.google.firebase.database.ValueEventListener;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
+import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
-    private static final int NB_MAX_BOX = 6;
-    private final DatabaseReference DB_STATS = FirebaseDatabase.getInstance().getReference("stats/box");
-    private final ArrayList<String> NAME_BOX_LIST = new ArrayList<>(Arrays.asList("number","color","meal","movie","anime","astronomy")); //TODO make Enum
-    private final ArrayList<String> STANDALONE_BOX_LIST = new ArrayList<>(Arrays.asList("number","color"));
-    private final ArrayList<Integer> BOX_ID_LIST = new ArrayList<>(Arrays.asList(R.id.box_1,R.id.box_2,R.id.box_3,R.id.box_4,R.id.box_5,R.id.box_6));
+public abstract class RandomBox {
+    public static String name = "randomBox";
+    private String apiUrl = "";
+    private Boolean hasJsonObject = true;
 
-    private MainActivity mainActivity;
-    private MediaPlayer rollDiceSound; // Dice sound effect
-    private MediaPlayer waterDropSound; // Water drop sound effect
-    private ArrayList<Integer> colorList;
+    /**
+     * The box no need api to work
+     */
+    public RandomBox(String name){
+        RandomBox.name = name;
+    }
 
-    private SensorManager mSensorManager; // object to access the device's sensors
-    private float mAccel; // acceleration apart from gravity
-    private float mAccelCurrent; // current acceleration including gravity
-    private float mAccelLast; // last acceleration including gravity
-
-    private static final HashMap<String, String> statsList = new HashMap<>();
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.mainActivity = this;
-
-        this.rollDiceSound = MediaPlayer.create(this, R.raw.roll_dice);
-        this.waterDropSound = MediaPlayer.create(this, R.raw.water_drop);
-
-        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-        mAccel = 0.00f;
-        mAccelCurrent = SensorManager.GRAVITY_EARTH;
-        mAccelLast = SensorManager.GRAVITY_EARTH;
-
-        // Collect the array in the colors.xml as an array of int and put it into a ArrayList to shuffle
-        int[] colorPalette = getResources().getIntArray(R.array.palette);
-        this.colorList = new ArrayList<>();
-        for(int color : colorPalette) {
-            colorList.add(color);
+    public RandomBox(String name, String apiUrl, Boolean hasJsonObject){
+        this(name);
+        this.apiUrl = apiUrl;
+        if(!apiUrl.isEmpty()){
+            this.hasJsonObject = hasJsonObject;
         }
-        //display box randomly, has to be run at the end of onCreate
-        shuffleBox();
-        updateBoxStats();
     }
 
     /**
-     * retrieves the statistics from the database
+     * Returns weather there is an API for this box or not
      */
-    private void updateBoxStats() {
-        DB_STATS.addValueEventListener(new ValueEventListener() {
+    public Boolean needAPI(){
+        return !apiUrl.isEmpty();
+    }
+
+    /**
+     * Returns the link of the API
+     */
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    /**
+     * Returns weather the API returns a JsonObject or not
+     */
+    public Boolean hasJsonObject(){
+        return hasJsonObject;
+    }
+
+    /**
+     * Set popup view for box which don't need API
+     * @param popupView the popupView to setup
+     */
+    public void setPopupView(View popupView){
+        Log.d("rb", "Set " + name + " Box in Popup View");
+    }
+
+    /**
+     * Set popup view for box which require API to work
+     * @param popupView the popupView to setup
+     * @param json the json response on success of the request to the API
+     */
+    public void setPopupView(View popupView, JSONObject json) throws JSONException {
+        Log.d("rb", "Set " + name + " Box in Popup View");
+    }
+
+    /**
+     * Set Info activity for box which require API
+     * @param infoView the InfoActivity
+     * @param json the json response on success of the request to the API
+     * @param generalFields map of general box fields name obtained via the database to show in Info activity //TODO make a better description
+     * @param additionalFields map of additional box fields name obtained via the database that will be display in the listView
+     */
+    public void setInfoView(InfoActivity infoView, JSONObject json, HashMap<String, String> generalFields, HashMap<String, String> additionalFields) throws JSONException {
+        if(!needAPI()){
+            Log.d("rb", name + " Box don't have more info");
+            return;
+        }
+        Log.d("rb", "Set " + name + " Box in Info Activity");
+
+        for(Map.Entry<String,String> pair: generalFields.entrySet()) {
+            switch (pair.getKey()){
+                case "title":
+                    String title = json.getString(pair.getValue());
+                    TextView titleView = infoView.findViewById(R.id.txtInfoContentTitle);
+                    titleView.setText(title); break;
+                case "subtitle":
+                    String subtitle = json.getString(pair.getValue());
+                    TextView subtitleView = infoView.findViewById(R.id.txtInfoContentSubtitle);
+                    setTextViewContent(subtitleView, subtitle); break;
+                case "description":
+                    String longText = json.getString(pair.getValue());
+                    TextView longtextView = infoView.findViewById(R.id.txtInfoContentLong);
+                    setTextViewContent(longtextView, longText); break;
+                case "picture":
+                    String imgUrl = json.getString(pair.getValue());
+                    ImageView imgView = infoView.findViewById(R.id.imgInfoContent);
+                    setImgViewURL(imgView, imgUrl); break;
+                default:
+                    Log.d("rb", pair.getKey() + " key doesn't exist in generalFields");
+            }
+        }
+
+        //set list view of optional info
+        ListView infoItems = infoView.findViewById(R.id.listInfo);
+        HashMap<String,String> itemList = new HashMap<>();
+        for(Map.Entry<String,String> pair: additionalFields.entrySet()) {
+            itemList.put(pair.getKey(),json.getString(pair.getValue()));
+        }
+        if(!itemList.isEmpty()){
+            Log.d("rb", "itemList " + itemList.toString());
+            infoItems.setAdapter(new BoxInfoItemAdapter(infoView, itemList));
+            infoItems.setVisibility(View.VISIBLE);
+        }
+    }
+
+    /**
+     * Returns the data for the box
+     * @param popupView : the view
+     * @param infoActivity : the data from the other page
+     */
+    public JsonObjectRequest makeJsonObjectRequest(View popupView, Intent infoActivity){
+        return new JsonObjectRequest(Request.Method.GET, apiUrl, null, new Response.Listener<JSONObject>() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                Log.d("rb", "Update box statistics");
-                statsList.clear();
-                for(DataSnapshot child: snapshot.getChildren()){
-                    statsList.put(child.getKey(), child.getValue().toString());
+            public void onResponse(JSONObject response) {
+                try {
+                    //Intent
+                    infoActivity.putExtra("jsonResponse", response.toString());
+                    Log.d("rb", name + " JSON = " + response.toString());
+                    setPopupView(popupView, response);
+                } catch (JSONException e) {
+                    Log.e("rb", name + " JSONObject Error : " +  e.getMessage());
                 }
             }
-
+        }, new Response.ErrorListener() {
             @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Log.d("rb", "cancel update stats : " + error.getMessage());
+            public void onErrorResponse(VolleyError error) {
+                Log.e("rb", name + " JSONObject Request FAIL : " + error.getMessage());
             }
         });
     }
 
     /**
-     * Shows in a popup the content of the box
-     * @param view : the view that called the method
+     * Returns the data for the box
+     * @param popupView : the view
+     * @param infoActivity : the data from the other page
      */
-    public void onClickBtnBox(View view){
-        Button btn = (Button) view;
-        waterDropSound.start();
-        String box_name = btn.getText().toString();
-        Log.d("rb", "Open " + box_name + " box");
-        if(!NAME_BOX_LIST.contains(box_name)){
-            Log.d("rb", "Unknown box name");
-            return;
-        }
-
-        //increment stat of the selected box
-        DB_STATS.child(box_name).setValue(ServerValue.increment(1));
-
-        //create & set intent
-        Intent infoActivity = new Intent(getApplicationContext(), InfoActivity.class);
-        infoActivity.putExtra("boxName", box_name);
-
-        //create box popup builder
-        AlertDialog.Builder builder = new AlertDialog.Builder(this); //main activity
-
-        // set the custom layout
-        final View popupView = getLayoutInflater().inflate(R.layout.popup,null);
-        builder.setView(popupView);
-
-        //update custom layout components
-        TextView title = popupView.findViewById(R.id.txtPopTitle);
-        title.setText(box_name);
-
-        RandomBox randomBox = RandomBoxFactory.buildBox(box_name);
-        if(randomBox == null){
-            Log.d("rb", "RandomBox IS NULL");
-            return;
-        }
-
-        if(!randomBox.needAPI()){
-            randomBox.setPopupView(popupView);
-        }
-        else {
-            // Instantiate the RequestQueue.
-            RequestQueue queue = Volley.newRequestQueue(this);
-            // Add the request to the RequestQueue.
-            if(randomBox.hasJsonObject()){
-                queue.add(randomBox.makeJsonObjectRequest(popupView, infoActivity));
-            } else {
-                queue.add(randomBox.makeJsonArrayRequest(popupView, infoActivity));
-            }
-        }
-
-        if(!STANDALONE_BOX_LIST.contains(box_name)){
-            builder.setNeutralButton("Get More Info", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    startActivity(infoActivity);
-                    mainActivity.finish();
+    public JsonArrayRequest makeJsonArrayRequest(View popupView, Intent infoActivity){
+        return new JsonArrayRequest(Request.Method.GET, apiUrl, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    //Intent
+                    infoActivity.putExtra("jsonResponse", response.getJSONObject(0).toString());
+                    Log.d("rb", name + " JSON = " + response.getJSONObject(0).toString());
+                    setPopupView(popupView, response.getJSONObject(0));
+                } catch (JSONException e) {
+                    Log.e("rb", name + " JSONArray Error : " +  e.getMessage());
                 }
-            });
-        }
-
-        //display popup
-        AlertDialog popup = builder.create();
-        popup.show();
-    }
-
-    /**
-     * Shuffles the boxes position by clicking on the logo.
-     * @param view : the view that called the method
-     */
-    public void onClickLogo(View view){
-        shuffleBox();
-    }
-
-    /**
-     * Creates the popup with statistics when the button stats is clicked on
-     * @param view : the view that called the method
-     */
-    public void onClickStats(View view) {
-        Log.d("rb", "Show stats in a ListView" + statsList.toString());
-        //create box stats popup builder
-        AlertDialog.Builder builder = new AlertDialog.Builder(mainActivity);
-        //set the custom layout
-        final View statsPopup = getLayoutInflater().inflate(R.layout.popup,null);
-        builder.setView(statsPopup);
-        TextView title = statsPopup.findViewById(R.id.txtPopTitle);
-        title.setText(R.string.stats);
-        //set stats ListView
-        ListView statsListView = statsPopup.findViewById(R.id.listViewStats);
-        statsListView.setAdapter(new BoxInfoItemAdapter(this, statsList));
-        statsListView.setVisibility(View.VISIBLE);
-        //display popup
-        AlertDialog popup = builder.create();
-        popup.show();
-    }
-
-    /**
-     * Shuffles the boxes position
-     */
-    public void shuffleBox(){
-        //Makes a rolling dice sound
-        Log.d("rb", "Rolling Dice");
-        rollDiceSound.start();
-
-        Log.d("rb", "Shuffle All Box");
-        Collections.shuffle(NAME_BOX_LIST);
-        Collections.shuffle(colorList);
-
-        Button btnBox;
-        for(int i = 0; i < NB_MAX_BOX; i++){
-            btnBox = findViewById(BOX_ID_LIST.get(i));
-            btnBox.setText(NAME_BOX_LIST.get(i));
-            btnBox.setBackgroundTintList(ColorStateList.valueOf(colorList.get(i)));
-        }
-    }
-
-    private final SensorEventListener mSensorListener = new SensorEventListener() {
-        /**
-         * Detects the shake motion and call shuffleBox()
-         * @param se : the SensorEvent passed in parameter
-         */
-        public void onSensorChanged(SensorEvent se) {
-            float x = se.values[0];
-            float y = se.values[1];
-            float z = se.values[2];
-            mAccelLast = mAccelCurrent;
-            mAccelCurrent = (float) Math.sqrt(x*x + y*y + z*z);
-            float delta = mAccelCurrent - mAccelLast;
-            mAccel = mAccel * 0.9f + delta; // perform low-cut filter
-
-            if (mAccel > 20) {
-                Log.d("rb", "Device shaken");
-                shuffleBox();
             }
-        }
-
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // ignore
-        }
-    };
-
-    @Override
-    protected void onResume() {
-        // Activates the accelerometer
-        super.onResume();
-        mSensorManager.registerListener(mSensorListener, mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("rb", name + " JSONArray Request FAIL : " + error.getMessage());
+            }
+        });
     }
 
-    @Override
-    protected void onPause() {
-        // Deactivates the accelerometer to saves resources
-        mSensorManager.unregisterListener(mSensorListener);
-        super.onPause();
+    /**
+     * Sets the view
+     * @param textViewById : the TextView
+     * @param text : the text that is shown in the view
+     */
+    public static void setTextViewContent(TextView textViewById, String text) {
+        textViewById.setText(text);
+        textViewById.setVisibility(View.VISIBLE);
     }
+
+    /**
+     * Sets the view
+     * @param textViewById : the TextView
+     * @param url : the image
+     */
+    public static void setImgViewURL(ImageView textViewById, String url) {
+        if (TextUtils.isEmpty(url)){
+            Log.d("rb", "The url is empty, can't set ImgView");
+            return;
+        }
+        Picasso.get().load(url).error(R.drawable.btn_blackheart).into(textViewById);
+        textViewById.setVisibility(View.VISIBLE);
+    }
+
 }
